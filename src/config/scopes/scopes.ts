f773138ee26cba00ca4e866498e5356e289c5b03
Nathan Fightmaster <nmfightmaster@gmail.com>
2026-06-13 09:36:22 -0700

    chore: remove potential circular dependency

--- src/config/scopes/scopes.ts
+++ src/config/scopes/scopes.ts
@@ -1,7 +1,5 @@
 import scopes from './scopes.json' with { type: "json" };
-import logger from '../../loaders/logger.js';
 
  export function getScopes() {
-    logger.debug('EVE Scopes loaded successfully');
     return scopes.join(' ');
  }
